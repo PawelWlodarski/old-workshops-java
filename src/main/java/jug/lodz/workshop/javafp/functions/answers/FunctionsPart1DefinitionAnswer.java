83c4a1f5d61e7fee99936b6233bd43748a8facef
pawel <pawel@domena.pl>
2016-06-20 17:03:37 +0200

    [functions] part 1 completed

---
+++ src/main/java/jug/lodz/workshop/javafp/functions/answers/FunctionsPart1DefinitionAnswer.java
@@ -0,0 +1,84 @@
+package jug.lodz.workshop.javafp.functions.answers;
+
+import javaslang.Tuple;
+import javaslang.Tuple1;
+import javaslang.Tuple2;
+import javaslang.Tuple3;
+import jug.lodz.workshop.Printer;
+
+import java.util.function.Function;
+
+/**
+ * Created by pawel on 19.06.16.
+ */
+public class FunctionsPart1DefinitionAnswer implements Printer{
+
+    private static FunctionsPart1DefinitionAnswer lab= new FunctionsPart1DefinitionAnswer();
+
+    public static void main(String[] args) {
+        lab.demo();
+    }
+
+    private void demo(){
+        println("\n\n JAVA 8 FUNCTIONS INTRO \n");
+
+        println("  +FUNCTIONS DEFINITION");
+
+        Function<Integer,Integer> f= i -> i+1;
+
+        println("    * apply f to 1 : "+f.apply(1));
+        println("    * apply f to 2 : "+f.apply(2));
+        println("    * apply f to 3 : "+f.apply(3));
+
+        Function<String,Integer> parseStringLong=new Function<String, Integer>() {
+            @Override
+            public Integer apply(String s) {
+                return Integer.parseInt(s);
+            }
+        };
+
+        Function<String,Integer> parseStringMedium= (String s1)->{
+            int parsed = Integer.parseInt(s1);
+            return parsed;
+        };
+
+        //SIMPLIFICATION
+        //* remove type
+        //* make one-liner
+        //* remove return and bracers
+
+        Function<String,Integer> parseString= s1->Integer.parseInt(s1);
+
+        println("  +FUNCTIONS COMPOSITION");
+
+        Function<String, Integer> parseStringAddOne = parseString.andThen(f);
+
+        Function<String, Integer> parseStringAddOneComposed = f.compose(parseString);
+
+
+        println("    * composed of '1' : "+parseStringAddOne.apply("1"));
+        println("    * composed of '2' : "+parseStringAddOne.apply("2"));
+
+
+
+        println("  +TUPLES");
+        Tuple1<String> t1 = Tuple.of("value");
+        Tuple2<String, Integer> t2 = Tuple.of("value", 2);
+        Tuple3<String, Integer, Boolean> t3 = Tuple.of("value", 3, true);
+
+        println("    * tuple of 3 elements : ["+t3._1+","+t3._2+","+t3._3+"]");
+
+        println("  +CLOSURE AND EFFECTIVELY FINAL");
+
+        double taxRate=0.23;
+
+        Function<Integer,Double> gross=net -> net+(taxRate*net);
+
+        //Illegal
+//        Function<Integer,Double> grossInvalid=net -> {
+//            taxRate=0.5;
+//            return net+(taxRate*net);
+//        };
+
+    }
+}
---
+++ src/test/java/jug/lodz/workshop/javafp/functions/exercises/FunctionsPart1DefinitionExercises.java
@@ -0,0 +1,143 @@
+package jug.lodz.workshop.javafp.functions.exercises;
+
+import javaslang.Tuple;
+import javaslang.Tuple2;
+import org.junit.Test;
+
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by pawel on 19.06.16.
+ */
+public class FunctionsPart1DefinitionExercises {
+
+    // LEVEL 1
+
+    /**
+     *
+     * Define two simple functions so that test pass
+     */
+    @Test
+    public void defineSimpleFunctions() throws Exception {
+        Function<Integer,Integer> square=null;
+        Function<Integer,Boolean> isEven=null;
+
+        assertThat(square.apply(2)).isEqualTo(4);
+        assertThat(square.apply(3)).isEqualTo(9);
+
+        assertThat(isEven.apply(1)).isFalse();
+        assertThat(isEven.apply(2)).isTrue();
+
+    }
+
+    /**
+     * Define two simple functions on tuple so that tests pass
+     * tuple._1 - returns first element
+     * tuple._2 - returns second element
+     */
+    @Test
+    public void operationsOnTuple() throws Exception {
+        Function<Tuple2<String,Integer>,Integer> snd= null;
+        Function<Tuple2<String,Integer>,Tuple2<Integer,String>> swap=null;
+
+
+        assertThat(snd.apply(Tuple.of("a",1))).isEqualTo(1);
+        assertThat(snd.apply(Tuple.of("b",2))).isEqualTo(2);
+
+        assertThat(swap.apply(Tuple.of("a",1))).isEqualTo(Tuple.of(1,"a"));
+        assertThat(swap.apply(Tuple.of("b",2))).isEqualTo(Tuple.of(2,"b"));
+    }
+
+    /**
+     * Complete 3 functions so their composition passes tests
+     * fst -> return first element of a tuple
+     * parse -> parse string to int
+     * square -> compute square of input
+     */
+    @Test
+    public void composition() throws Exception {
+        Function<Tuple2<String,Integer>,String> fst=null;
+        Function<String,Integer> parse=null;
+        Function<Integer,Integer> square=null;
+
+        Function<Tuple2<String, Integer>, Integer> squareFst = fst.andThen(parse).andThen(square);
+
+        assertThat(squareFst.apply(Tuple.of("2",1))).isEqualTo(4);
+        assertThat(squareFst.apply(Tuple.of("3",1))).isEqualTo(9);
+
+
+    }
+
+    @Test
+    public void closure() throws Exception {
+        int counter=20;
+
+        Function<Integer,Integer> addToCounter=null;
+
+        assertThat(addToCounter.apply(5)).isEqualTo(25);
+        assertThat(addToCounter.apply(7)).isEqualTo(27);
+
+    }
+
+    //LEVEL2
+
+    /**
+     * Practice Function as a value
+     * @throws Exception
+     */
+    @Test
+    public void testCustomAndThen() throws Exception {
+        Function<String, Double> squareStr = andThen(s -> Integer.parseInt(s), i -> Math.sqrt(i));
+
+        assertThat(squareStr.apply("4")).isEqualTo(2.0);
+        assertThat(squareStr.apply("9")).isEqualTo(3.0);
+    }
+
+    /**
+     *
+     * You can use "andThen" and "compose" from Function interface
+     *
+     * @param f1 -> String to int
+     * @param f2 -> int to double
+     * @return
+     */
+    private Function<String,Double> andThen(Function<String,Integer> f1,Function<Integer,Double> f2){
+        return null;
+    }
+
+
+    //LEVEL3
+
+
+    /**
+     *  implement andThen generic
+     */
+//    @Test
+//    public void testAndThenGeneric() throws Exception {
+//        Function<String, Boolean> isStrEven = andThenGeneric((String s) -> Integer.parseInt(s), i -> i %2==0);// Explicit Type needed!
+//        Function<Integer, String> squareAndToString = andThenGeneric((Integer i) -> i * i, i -> "result is : " + i);
+//
+//
+//        assertThat(isStrEven.apply("2")).isTrue();
+//        assertThat(isStrEven.apply("3")).isFalse();
+//
+//
+//        assertThat(squareAndToString.apply(3)).isEqualTo("result is : 9");
+//        assertThat(squareAndToString.apply(4)).isEqualTo("result is : 16");
+//
+//    }
+
+    /**
+     *
+     *define generic andThen function
+     *
+     */
+//    private <A,B,C> Function<???,???> andThenGeneric(Function<???,???> f1, Function<???,???> f2){
+//        return ???
+//    }
+
+
+
+}
